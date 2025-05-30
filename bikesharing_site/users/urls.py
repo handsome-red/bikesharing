6047bf5a7a3c0a1cfd32e818e755d73c2e7770f1
gabrilau <egorovandrey314@gmail.com>
2025-05-31 00:40:41 +0300

    i love Aigul

--- bikesharing_site/users/urls.py
+++ bikesharing_site/users/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('login/', views.LoginUser.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('password-change/', views.UserPasswordChange.as_view(), name='password_change'),
-    path('password-change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password-change/done/', PasswordChangeDoneView.as_view(template_name="users/password_change_done.html"), name='password_change_done'),
     path('register/', views.RegisterUser.as_view(), name='register'),
     path('profile/', views.ProfileUser.as_view(), name='profile'),
 ]
